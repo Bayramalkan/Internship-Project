92035b3893f4caab5f6be595f9247b0235946258
Bayram ALKAN <61650997+Bayramalkan@users.noreply.github.com>
2022-08-03 16:54:10 +0300

    trying ssr

--- components/card/CardList.js
+++ components/card/CardList.js
@@ -4,18 +4,39 @@ import styles from "./Card.module.css";
 // const data = require("../../data/response.json");
 import axios from "axios";
 
-function CardList() {
-  const axios = require("axios");
-  const [data, setData] = useState([]);
+export async function getServerSiderProps() {
+  // response.setHeader("Cache-Control", "s-maxage=1, stale-while-revalidate=59");
+  const response = await axios.get(
+    "https://monster-p.mncdn.com/cckeyboardapp/response.json"
+  );
+  // console.log(response.data);
+  return { props: { data: response.data } };
+}
 
+function CardList(props) {
+  // const axios = require("axios");
+  const [result, setResult] = useState([]);
   useEffect(() => {
-    axios
-      .get("https://monster-p.mncdn.com/cckeyboardapp/response.json")
-      .then((response) => {
-        console.log(response.data);
-        setData(response.data);
-      });
+    // setResult(data);
+    getServerSiderProps().then((res) => {
+      setResult(res.props.data);
+      // console.log(res.props.data);
+      // console.log(result);
+    });
   }, []);
+
+  // console.log(data);
+
+  // const [data, setData] = useState([]);
+
+  // useEffect(() => {
+  //   axios
+  //     .get("https://monster-p.mncdn.com/cckeyboardapp/response.json")
+  //     .then((response) => {
+  //       console.log(response.data);
+  //       setData(response.data);
+  //     });
+  // }, []);
 
   // const ids = data?.data?.products.map((item) => {
   //   return (ids = item.catIds);
@@ -23,14 +44,14 @@ function CardList() {
 
   // console.log(ids);
 
-  const result = data?.data?.products.filter((item) => {
+  const ProductResult = result?.data?.products.filter((item) => {
     return item.catIds.includes("6224");
   });
-  console.log("result: ", result);
+  console.log("resultaaaa: ", ProductResult);
 
   return (
     <div className={styles.cardListwrapper}>
-      {result?.map((item, index) => {
+      {ProductResult?.map((item, index) => {
         return <Card key={index} item={item} index={index} />;
       })}
     </div>
